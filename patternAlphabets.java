7696353b63e78949b1ec717c0c396c718fdbf6c8
Junaith999 <139519850+Junaith999@users.noreply.github.com>
2025-02-18 23:05:31 +0530

    Add files via upload

---
+++ patternAlphabets.java
@@ -0,0 +1,87 @@
+import java.util.*;
+public class patternAlphabets{
+	public static void main(String[] args) {
+		char rows='E';
+		for(char i='A'; i<=rows; i++){
+			for(char j='A'; j<=i; j++){
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
